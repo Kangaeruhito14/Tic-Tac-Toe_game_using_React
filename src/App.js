2add90442d0b225ca3130cef5c8b685f307dd4b6
Kangaeruhito14 <ap1484480@gmail.com>
2024-10-30 14:07:49 +0600

    Version-01 (Work by Anup Podder)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TicTacToe  from './Components/TicTacToe/TicTacToe';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TicTacToe/>
+      </div>
+    );
+  }
+}
+
+export default App;
